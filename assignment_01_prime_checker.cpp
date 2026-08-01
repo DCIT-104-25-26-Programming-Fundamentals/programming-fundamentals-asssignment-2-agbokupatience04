ca0bc2b354b689c2cd2af1d32346224e9bf1f08e
agbokupatience04 <abgokupatience04@gmail.com>
2026-08-01 15:05:31 +0000

    Complete prime number checker

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -35,5 +35,37 @@
 // =============================================================================
 
 #include <iostream>
+
 using namespace std;
+bool isPrime(int number){
+    if (number<= 1){
+        return false;
+    }
+  for (int i = 2; i <= number/2; i++){
+    if (number % i== 0){
+    return false;
+    }
+  }  
+
+return true;
+}
+    int main(){
+        int number;
+
+        cout << number << "Enter a number:";
+        cin >> number;
+
+        bool result = isPrime(number);
+
+        if (result) {
+            cout << number << " is a prime number." << endl;
+            }else {
+                cout << number <<"is not a prime number." << endl;
+            }
+            return 0;
+        }
+
+    
+
+
 
